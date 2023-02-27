67a62ba1de081a4ba41c799500e288f0e3b23e85
AidaMillatiMardiana <aidadiana2702@gmail.com>
2023-02-27 09:39:12 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+const fileName = 'script.js';
+const lineNumber = 42;
+
+const errorMessage = `Error in ${fileName} at line ${lineNumber}: something went wrong.`;
+
+console.log(errorMessage); // Output: Error in script.js at line 42: something went wrong.
